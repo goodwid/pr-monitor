594b30a5c9a8eac0098a7cda05c7498b2934a16e
goodwid <goodwid@gmail.com>
2019-06-04 08:25:01 -0700

    Refactored to have multiple display options, command-line config tools

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# Pull Requests
+# Github pull request monitor
 
-This started out as a [BitBar](https://getbitbar.com/) plugin but I've spun off another script that outputs direct to the console for general purpose usage.
+This started out as a [BitBar](https://getbitbar.com/) plugin. I had spun off another script that outputs direct to the console for general purpose usage, then realized it needed more work.
+
+It has evolved into a single terminal app that has configuration options, storing the Github token and repositories to track.
+
 
 To use this project with BitBar:
 
-- clone the repo, 
-- copy `config.json.example` to `config.json` 
-- add in the repos to monitor and your github key.
-- create a symbolic link from your bitbar dir to `index.js`:  `ln -s /path/to/repo/index.js /path/to/bitbar/github-pr-5m.js'
+- add your github token  `pr-monitor config -g <token>`
+- add your repository `pr-monitor config -a <org>/<repo>`
+- set your default display `pr-monitor config -d bitBar`
+- create a symbolic link from your bitbar dir to `index.js`:  `ln -s pr-monitor /path/to/bitbar/github-pr-5m.js'
 
 For other use, follow the first three steps, and roll your own monitoring (conky? rainmeter?)
--- index.js
+++ index.js
@@ -1,53 +1,113 @@
-#!/usr/bin/env /usr/local/bin/node
+#!/usr/bin/env node
 
-const config = require('./config.json');
-const { repos, github_key } = config;
-const request = require('./lib/request')(github_key);
-const bitbar = require('bitbar');
+const program = require('commander');
+const Preferences = require('preferences');
+let app = 'pr-monitor';
+const prefs = new Preferences(app);
+const bitBarFormat = require('./lib/data-format/bitbar');
+const terminalFormat = require('./lib/data-format/terminal');
+const jsonFormat = require('./lib/data-format/json');
+const formatOptions = { bitBar: bitBarFormat, terminal: terminalFormat, json: jsonFormat };
 
-async function getData(repo) {
-  let res;
-  try {
-    res = await request.get(`/repos/${repo}/pulls`);
-  }
-  catch (e) {
-    console.log(`Error: ${e.code}|color="red" refresh="true"`);
-    process.exit(1);  
-  }
-  return res.data;
-}
+// const getData = require('./lib/get-data');
+const { alert, alertErr } = require('./lib/cli-tools');
 
-const promises = repos.map(repo => getData(repo));
-const title = {
-  text: ''
-};
-
-const output = [title, bitbar.separator];
-
-Promise.all(promises).then(repos => {
-  if(!repos) return
-  let count = 0;
-  repos.forEach(repo => {
-    const submenu = repo.map(pr => {
-      if(!pr) return null; 
-      count++;
-      const base = pr.base;
-      const name = base.repo.name;
-      return { text: `${pr.user.login} on ${pr.head.ref}`, href: pr.html_url, name }
-    })
-    if(submenu.length) {
-      const text = submenu[0].name;
-      const block = {text , color: 'yellow', submenu };
-      output.push(block);
+program
+  .command('config')
+  .option('-g --githubToken <githubToken>')
+  .option('-a --addRepo <repoToAdd>')
+  .option('-r --removeRepo <repoToRemove>')
+  .option('-d --defaultFormat <format>', 'Display format')
+  .description('Configure the application')
+  .action(({ githubToken, addRepo, removeRepo, defaultFormat }) => {
+    if (defaultFormat) {
+      if (['bitBar', 'terminal', 'json'].indexOf(defaultFormat) === -1) {
+        alertErr('Default must be one of the following options: bitBar, terminal, json.');
+        process.exit(1);
+      }
+      alert(`Default is set to ${defaultFormat}`);
+      prefs.defaultFormat = defaultFormat;
     }
+    if (githubToken) {
+      prefs.githubToken = githubToken;
+      alert('Github Token added.');
+    }
+    if (addRepo) {
+      const repos = prefs.repos || [];
+      if (repos.indexOf(addRepo) === -1) {
+        repos.push(addRepo);
+        prefs.repos = repos;
+        alert(`${addRepo} has been added.`);
+      } else {
+        alertErr(`${addRepo} is already configured.`);
+      }
+    }
+    if (removeRepo) {
+      const repos = prefs.repos || [];
+      const found = repos.indexOf(removeRepo);
+      if (found === -1) {
+        alertErr(`Repo ${removeRepo} not found.`);
+      } else {
+        repos.splice(found, 1);
+        alert(`${removeRepo} has been removed.`);
+      }
+    }
+    process.exit(0);
   });
-  title.color = count <= 1 ? 'green':
-                count <= 4 ? 'yellow' :
-                'red';
-  title.text = `PRs: ${count}`;
 
-  bitbar(output)
-})
-  .catch(console.log);
+program
+  .command('show-config')
+  .option('-s --showKeys')
+  .description('Display current configuration data.')
+  .action((cmd) => {
+    if (cmd.showKeys) prefs.githubToken 
+      ? alert(`Current Github token is ${prefs.githubToken}`)
+      : alertErr('There is no Github token configured.');
+    if (prefs.repos) {
+      alert('The following repos are stored: ');
+      prefs.repos.forEach(repo => alert(' - ' + repo));
+    }
+    process.exit(0);
 
+  });
+  
+program
+  .command('clear')
+  .description('Clear current configuration data.')
+  .option('-g --githubToken')
+  .option('-r --repos')
+  .action((options) => {
+    const { githubOrg, repos } = options;
+    if (repos) {
+      prefs.githubOrg = undefined;
+      alertErr('Github repos have been cleared.');
+    }
+    if (githubToken) {
+      prefs.githubToken = undefined;
+      alertErr('Github token has been cleared.');
+    }
+    if (!githubOrg && !githubToken) {
+      prefs.githubOrg = undefined;
+      prefs.githubToken = undefined;
+      alertErr('all configurations have been removed.');
+    }
+    process.exit(0);
 
+  });
+
+program
+  .description('Show current pull requests for all configured repos.')
+  .option('-b --bitBar', 'Outputs data in a format usable by bitBar.')
+  .option('-t --terminal', 'Outputs data to the terminal.')
+  .option('-j --json', 'Outputs data in JSON format.')
+  .parse(process.argv);
+
+const { bitBar, terminal, json } = program;
+const { githubToken, repos, defaultFormat } = prefs;
+const display = 
+  bitBar ? 'bitBar' :
+    terminal ? 'terminal' :
+      json ? 'json' :
+        defaultFormat || 'terminal';
+const formatter = formatOptions[display];
+formatter(githubToken, repos);
